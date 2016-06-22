62e520b0274169c616b00978101ab0ec2d684fc1
carter.fendley <carter.fendley@gmail.com>
2016-06-22 13:50:45 -0400

    Bare bones ele test

---
+++ electrical_test/components/swervemodule.py
@@ -0,0 +1,83 @@
+import wpilib
+from networktables import NetworkTable
+
+MAX_VOLTAGE = 5
+MAX_TICK = 4050
+MAX_DEG = 360
+
+class SwerveModule:
+    
+    def __init__(self, driveMotorPort, rotateMotorPort, encoderPort, SDPrefix="SwerveModule"):
+        '''
+        :param driveMotorPort: pwm port number of VictorSP Motor
+        :param rotateMotorPort: pwm port number of VictorSP Motor
+        :param encoderPort: analog in port number of Absolute Encoder
+        '''
+        
+        self.sd = NetworkTable.getTable('SmartDashboard')
+        self.sd_prefix = SDPrefix
+        
+        self.driveMotor = wpilib.VictorSP(driveMotorPort)
+        self.rotateMotor = wpilib.VictorSP(rotateMotorPort)
+        self.encoder = wpilib.AnalogInput(encoderPort)
+        
+        self.encoder_zero = 0
+        
+        self.pid_controller = wpilib.PIDController(0.0001, 0.001, 0.0, self.encoder, self.rotateMotor)
+        self.pid_controller.setTolerance(20)
+        self.pid_controller.setContinuous()
+        self.pid_controller.enable()
+        
+        self.requested_voltage = 0;
+        
+        self.zero_encoder()
+    
+    def get_degrees(self):
+        volt =  (self.encoder.getVoltage() - self.encoder_zero)
+        deg = (volt/4050)*360
+        deg = deg
+        
+        if deg < 0:
+            deg = 360+deg;
+        
+        return deg
+    
+    @staticmethod
+    def tick_to_deg(tick):
+        deg = (tick/4050)*360
+        return deg
+    
+    @staticmethod
+    def deg_to_voltage(deg):
+        volt = (deg/360)*5
+        return volt
+    
+    @staticmethod
+    def deg_to_tick(deg):
+        volt = (deg/360)*4050
+        return volt
+    
+    def zero_encoder(self):
+        self.encoder_zero = self.encoder.getVoltage()
+    
+    def set_deg(self, value):
+        value = value % 360
+        '''
+        if(abs(value - self.get_degrees()) > 90):
+            value -= 180'''
+        self.requested_voltage = self.deg_to_voltage(value)
+    
+    def doit(self):
+        self.pid_controller.setSetpoint(self.requested_voltage)
+        self.update_smartdash()
+    
+    def update_smartdash(self):
+        self.sd.putNumber("drive/%s | Requested Voltage" % self.sd_prefix, self.requested_voltage)
+        self.sd.putNumber("drive/%s | Voltage" % self.sd_prefix, self.encoder.getVoltage())
+        self.sd.putNumber("drive/%s | Tick " % self.sd_prefix, self.encoder.getValue())
+        self.sd.putNumber("drive/%s | Degrees" % self.sd_prefix, self.get_degrees())
+        self.sd.putNumber("drive/%s | Degrees" % self.sd_prefix, self.pid_controller.onTarget())
+        
+        
+        
+        
---
+++ electrical_test/robot.py
@@ -0,0 +1,76 @@
+import wpilib
+from networktables import NetworkTable
+
+from components.swervemodule import SwerveModule
+
+class MyRobot(wpilib.SampleRobot):
+    
+    def robotInit(self):
+        
+        self.sd = NetworkTable.getTable('SmartDashboard')
+        wpilib.PIDController
+        self.encoders = [];
+        
+        #driveMotorPort, rotateMotorPort, encoderPort
+        self.rr_module = SwerveModule(3,2,3, SDPrefix="RR Module")
+        self.lr_module = SwerveModule(7,6,1, SDPrefix="LR Module")
+        self.rf_module = SwerveModule(4,5,2, SDPrefix="RF Module")
+        self.lf_module = SwerveModule(1,0,0, SDPrefix="LR Module")
+        
+        '''
+        self.lr_encoder = wpilib.AnalogInput(1)
+        self.lr_rotate_motor = wpilib.VictorSP(7)
+        self.lr_move_motor = wpilib.VictorSP(6)
+        
+        self.rf_encoder = wpilib.AnalogInput(2)
+        self.lf_rotate_motor = wpilib.VictorSP(5)
+        self.lf_move_motor = wpilib.VictorSP(4)
+        
+        self.lf_encoder = wpilib.AnalogInput(0)
+        self.lf_rotate_motor = wpilib.VictorSP(0)
+        self.lf_move_motor = wpilib.VictorSP(1)
+        '''
+        
+        self.components = {
+            'lf_module': self.lf_module,
+            'rf_module': self.rf_module,
+            'lr_module': self.lr_module,
+            'rr_module': self.rr_module,
+        }
+        
+        self.joystick = wpilib.Joystick(0)
+        self.wheel_rotation = 0
+        
+        '''
+        for i in range(0,4):
+            self.encoders.append(wpilib.AnalogInput(i))'''
+    
+    def disabled(self):
+        wpilib.Timer.delay(.01)
+        
+    def operatorControl(self):
+        while self.isOperatorControl() and self.isEnabled():
+            '''
+            for i, encoder in enumerate(self.encoders):
+                self.sd.putNumber('Encoder #%s' % i, encoder.getValue())
+                self.sd.putNumber('Encoder Angle #%s' % i, self.tickToDeg(encoder.getValue()))
+                self.sd.putNumber('Encoder Voltage #%s' % i, encoder.getVoltage())
+            '''
+            
+            self.wheel_rotation += self.joystick.getAxis(0);
+            
+            self.rr_module.set_deg(self.wheel_rotation)
+            self.lr_module.set_deg(self.wheel_rotation)
+            self.rf_module.set_deg(self.wheel_rotation)
+            self.lf_module.set_deg(self.wheel_rotation)
+            
+            self.update()
+            wpilib.Timer.delay(0.005)
+    
+    def update (self):
+        for component in self.components.values():
+            component.doit()
+        
+
+if __name__ == '__main__':
+    wpilib.run(MyRobot)
