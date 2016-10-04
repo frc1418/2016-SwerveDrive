dd75e7cb101bae6f0c94c3f04d140a92c951afca
carter.fendley <carter.fendley@gmail.com>
2016-10-04 15:00:45 -0400

    Update docstrings

--- robot/components/swervemodule.py
+++ robot/components/swervemodule.py
@@ -11,34 +11,45 @@ class SwerveModule:
 
     def __init__(self, driveMotor, rotateMotor, encoderPort, SDPrefix="SwerveModule", zero=0.0, inverted=False, allow_reverse=False, debugging=False):
         '''
-        :param driveMotorPort: Motor object
-        :param rotateMotorPort: Motor object
+        Swerve drive module was written for a swerve drive train that uses absolute encoders for tracking wheel rotation.  
+        
+        :param driveMotor: Motor object
+        :param rotateMotor: Motor object
+        
         :param encoderPort: analog in port number of Absolute Encoder
+        
+        :param SDPrefix: a string used to differentiate modules when debugging
+        :param zero: The default zero for the encoder
+        :param inverted: boolean to invert the wheel rotation
+        
+        :param allow_reverse: If true allows wheels to spin backwards instead of rotating
+        
+        :param debugging: Prints more NT values if True
         '''
 
         #SmartDashboard
         self.sd = NetworkTable.getTable('SmartDashboard')
         self.sd_prefix = SDPrefix
 
+        #Motors
         self.driveMotor = driveMotor
         self.driveMotor.setInverted(inverted)
         self.rotateMotor = rotateMotor
+        
+        self.requested_voltage = 0
+        self.requested_speed = 0
+        
+        #Encoder
         self.encoder = wpilib.AnalogInput(encoderPort)
-
-        self.encoder_zero = 0
+        self.encoder_zero = zero
 
         #PID
         self.pid_controller = wpilib.PIDController(1.2, 0.0, 0.0, self.encoder, self.rotateMotor)
-        #self.pid_controller.setTolerance(5)
         self.pid_controller.setContinuous()
         self.pid_controller.setInputRange(0.0, 5.0)
         self.pid_controller.enable()
 
-        self.requested_voltage = 0
-        self.requested_speed = 0
-
-        self.encoder_zero = zero
-        
+        #State variables
         self.allow_reverse = allow_reverse
         self.debugging = debugging
 
@@ -99,22 +110,29 @@ class SwerveModule:
         self.encoder_zero = self.encoder.getVoltage()
         
     def set_allow_reverse(self, value):
+        '''
+        Sets the ability for the wheels to add 180deg to the requested rotation
+        and invert speed rather than rotating over 90deg.
+        '''
         self.allow_reverse = value
         
     def set_debugging(self, value):
+        '''
+        Sets if the update_smartdash function will output more vars.
+        '''
         self.debugging = value
 
     def _set_deg(self, value):
         '''
         Rounds the value to within 360. Sets the requested rotate position (requested voltage).
-        Ment to be used only by the move function.
+        Intended to be used only by the move function.
         '''
         self.sd.putNumber('drive/%s/ Requested D' % self.sd_prefix, value)
         self.requested_voltage = ((self.degree_to_voltage(value)+self.encoder_zero) % 5)
 
     def move(self, speed, deg):
         '''
-        Sets the requested speed and roation of passed
+        Sets the requested speed and rotation of passed
         '''
         
         deg = deg % 360 #Prevents values past 360
@@ -131,10 +149,10 @@ class SwerveModule:
 
     def doit(self):
         '''
-        Uses the pid controler to get closer to the reqested postition.
+        Uses the PID controller to get closer to the requested position.
         Sets the speed requested of the drive motor.
 
-        Should be called every robot iteration.
+        Should be called every robot iteration/loop.
         '''
         self.pid_controller.setSetpoint(self.requested_voltage)
         self.driveMotor.set(self.requested_speed)
@@ -142,7 +160,7 @@ class SwerveModule:
 
     def update_smartdash(self):
         '''
-        Outputs a bunch on internal variables for debuging purposes.
+        Outputs a bunch on internal variables for debugging purposes.
         '''
         
         self.sd.putNumber("drive/%s/Degrees" % self.sd_prefix, self.voltage_to_degrees(self.get_voltage()))
@@ -150,7 +168,7 @@ class SwerveModule:
         if self.debugging:
             self.sd.putNumber("drive/%s/Requested Voltage" % self.sd_prefix, self.requested_voltage)
             self.sd.putNumber("drive/%s/Requested Speed" % self.sd_prefix, self.requested_speed)
-            self.sd.putNumber("drive/%s/Voltage" % self.sd_prefix, self.encoder.getVoltage())
+            self.sd.putNumber("drive/%s/Voltage" % self.sd_prefix, self.encoder.getVoltage()) #DO NOT USE self.get_voltage() here
             self.sd.putNumber("drive/%s/Zero" % self.sd_prefix, self.encoder_zero)
     
             self.sd.putNumber("drive/%s/PID" % self.sd_prefix, self.pid_controller.get())
